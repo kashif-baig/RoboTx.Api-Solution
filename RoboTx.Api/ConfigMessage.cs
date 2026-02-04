c6b8e5d1217bfc0815288562cae01559dd7b3ff6
Kashif Baig <m_kashif_baig@hotmail.com>
2026-02-04 18:53:24 +0000

    Removed redundant code in Config and ConfigMessage classes.

--- RoboTx.Api/ConfigMessage.cs
+++ RoboTx.Api/ConfigMessage.cs
@@ -2,23 +2,10 @@
 
 namespace RoboTx.Api
 {
-    internal enum MotorDriverType
-    {
-        NotSet,
-        MotorInIn,
-        MotorDirPwm,
-        L298
-    }
-
     internal class ConfigMessage : SerializableMessage
     {
         public const string MessageType = "CFG";
-        private bool? _displayIsInverse = null;
-        private bool? _displayIsRTL = null;
         private bool? _inputsinverted = null;
-        private bool? _beeperInverted = null;
-        MotorDriverType _motorDriverType = MotorDriverType.NotSet;
-        private bool? _motorUseEnabled = null;
         private bool? _inputEventsEnabled = null;
         private string _inputPins = string.Empty;
         private bool? _analogInputsEnabled = null;
@@ -26,42 +13,6 @@ namespace RoboTx.Api
         public ConfigMessage(StreamWriter sw) : base(sw)
         {
 
-        }
-
-        public void SetDisplayInverse(bool isInverse)
-        {
-            _displayIsInverse = isInverse;
-        }
-
-        public void SetDisplayRTL(bool isRTL)
-        {
-            _displayIsRTL = isRTL;
-        }
-
-        public void SetBeeperInverted(bool beeperInverted)
-        {
-            _beeperInverted = beeperInverted;
-        }
-
-        public void UseMotorDriverL298()
-        {
-            _motorDriverType = MotorDriverType.L298;
-        }
-
-        public void UseMotorDriverInIn()
-        {
-            _motorDriverType = MotorDriverType.MotorInIn;
-        }
-
-        public void UseMotorDriverDirPwm()
-        {
-            _motorDriverType = MotorDriverType.MotorDirPwm;
-        }
-
-        public void UseMotorDriver2DirPwm(bool useEnable)
-        {
-            _motorDriverType = MotorDriverType.MotorDirPwm;
-            _motorUseEnabled = useEnable;
         }
 
         public void InvertDigitalInputs(bool inverted, params int[] inputPins)
@@ -110,44 +61,11 @@ namespace RoboTx.Api
 
         protected override void OnSerialize()
         {
-            if (_displayIsInverse != null)
+            if (_inputsinverted != null)
             {
-                SerializeProperty(_displayIsInverse.Value ? "7SegInv" : "7SegNorm");
-            }
-            else if (_displayIsRTL != null)
-            {
-                SerializeProperty(_displayIsRTL.Value ? "7SegRTL" : "7SegLTR");
-            }
-            else if (_inputsinverted != null)
-            {
-                //SerializeProperty(_inputsinverted.Value ? "InpInv" : "InpNorm");
                 SerializeProperty("InpInv");
                 SerializeProperty(_inputsinverted.Value ? "T" : "F");
                 SerializeProperty(_inputPins);
-            }
-            else if (_beeperInverted != null)
-            {
-                SerializeProperty(_beeperInverted.Value ? "BprInv" : "BprNorm");
-            }
-            else if (_motorDriverType == MotorDriverType.MotorInIn)
-            {
-                SerializeProperty("InIn");
-                //if (_motorUseEnabled.HasValue)
-                //{
-                //    SerializeProperty(_motorUseEnabled.Value ? "T" : "F");
-                //}
-            }
-            else if (_motorDriverType == MotorDriverType.MotorDirPwm)
-            {
-                SerializeProperty("DirPwm");
-                if (_motorUseEnabled.HasValue)
-                {
-                    SerializeProperty(_motorUseEnabled.Value ? "T" : "F");
-                }
-            }
-            else if (_motorDriverType == MotorDriverType.L298)
-            {
-                SerializeProperty("L298");
             }
             else if (_inputEventsEnabled != null)
             {
